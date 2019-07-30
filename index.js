451c6337b1bb24e43ca0dc7df9fe30b1bc5d2377
Jim Strother <jim.strother@gmail.com>
2019-07-30 13:46:07 -0700

    second transpile

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
 "use strict";
 exports.__esModule = true;
-var message = 'Welcome Back';
+var message = 'Hello, World';
 console.log(message);
+var isBeginner = true;
+var total = 0;
+var name = 'Jim';
+var sentence = "My name is " + name + ".\nI am a beginner in Typescript";
+console.log(sentence);
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person1 = ['Jim', 41];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+console.log(c);
+var randomValue = 10;
+randomValue = true;
+randomValue = 'Jim';
+var myVariable1 = 10;
+console.log(myVariable1.name);
+myVariable1();
+myVariable1.toUpperCase();
+var myVariable2 = 10;
+function hasName(obj) {
+    return !!obj &&
+        typeof obj === 'object' &&
+        'name' in obj;
+}
+if (hasName(myVariable2))
+    console.log(myVariable2.name);
+// myVariable2(); throws error, but meant to illustrate that you can attempt to call type any as a function even if it isn't, while type unknown prevents this
+myVariable2.toUpperCase();
--- index.ts
+++ index.ts
@@ -3,3 +3,48 @@ export {};
 let message = 'Hello, World';
 
 console.log(message);
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Jim';
+
+let sentence: string = `My name is ${name}.
+I am a beginner in Typescript`;
+
+console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
+
+let person1: [string, number] = ['Jim', 41];
+
+enum Color {Red, Green, Blue};
+let c: Color = Color.Green;
+console.log(c);
+
+let randomValue: any = 10;
+randomValue = true;
+randomValue = 'Jim';
+
+let myVariable1: any = 10;
+console.log(myVariable1.name);
+myVariable1();
+myVariable1.toUpperCase();
+
+let myVariable2: unknown = 10;
+
+function hasName(obj: any): obj is {name: string} {
+    return !!obj &&
+        typeof obj === 'object' &&
+        'name' in obj;
+}
+if (hasName(myVariable2)) console.log(myVariable2.name);
+
+// myVariable2(); throws error, but meant to illustrate that you can attempt to call type any as a function even if it isn't, while type unknown prevents this
+(myVariable2 as string).toUpperCase();
